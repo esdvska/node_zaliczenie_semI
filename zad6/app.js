16dad02e5c3553d3274e896a73de1b99683177d8
Luczko <wlalukasz@gmail.com>
2022-01-31 19:46:15 +0100

    Dodanie 6 zad

---
+++ zad6/app.js
@@ -0,0 +1,12 @@
+// Napisz aplikację pozwalającą na przechowywanie w pliku listy zadań do wykonania (klasyczna lista TODO). Aplikacja powinna pozwalać na dodanie do listy nowego zadania, jak również wyświetlić zawartość całej listy. Przy uruchomieniu bez parametrów aplikacja powinna informować o możliwych parametrach wywołania.
+
+// zapis/odczyt wykonuj asynchronicznie
+// pamiętaj o obsłudze błędów
+// poinformuj użytkownika o poprawności wykonanych operacji
+// wydziel odczyt i zapis informacji do osobnych modułów
+// Sugeruje użyć modułu yargs z konstrukcją yargs.command.
+
+// Przykład wywołania programu:
+
+// > node app.js dodaj "napisac program na zaliczenie z NodeJS"
+// > node app.js lista
