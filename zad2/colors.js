2c5abd777c14181f1b944f2a89eec939ca1e28a9
Luczko <wlalukasz@gmail.com>
2022-02-10 17:53:39 +0100

    Poprawa drugiego zadania

--- zad2/colors.js
+++ zad2/colors.js
@@ -1,13 +1,13 @@
 const yargs = require("yargs");
 const colors = require("colors");
-
 const args = yargs.argv;
+
 (() => {
   if (args.rainbow) {
-    console.log(args.$0);
+    console.log(args.rainbow.rainbow);
   } else {
     console.log(
-      `Proszę podaj mi parametry wejściowe '--rainbow==Twój_ciąg_znaków'`
+      `Proszę podaj mi parametry wejściowe '--rainbow=Twój_ciąg_znaków'`
     );
   }
 })();
